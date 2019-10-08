9c476f4444dc3f6d1e41ba9475b309b2f596787b
Kay <khurrammohammed1990@gmail.com>
2019-10-08 01:02:31 +0100

    minimum balance updated for oyster

--- OysterCard/Oyster.cs
+++ OysterCard/Oyster.cs
@@ -10,6 +10,7 @@ namespace OysterCard
     {
         private double balance;
         private double maxLimit = 90.00;
+        private bool inJourney = false;
         public Oyster(double _balance)
         {
             balance = _balance;
@@ -17,11 +18,38 @@ namespace OysterCard
 
         public double oysterBalance { get => balance; set => balance = value; }
         public double maxOysterLimit { get => maxLimit; set => maxLimit = value; }
+        public bool inOysterJourney { get => inJourney; set => inJourney = value; }
 
         public double TopUp(double amount)
         {
-            this.oysterBalance += amount;
+            if ((oysterBalance+amount) > maxOysterLimit)
+            {
+                throw new Exception($"The maximum balance is {maxOysterLimit} GBP");
+            }
+                oysterBalance += amount;
+                return oysterBalance;
+        }
+
+        public double Deduct(double amount)
+        {
+            oysterBalance -= amount;
             return oysterBalance;
+        }
+
+        public bool TouchIn()
+        {
+            if (oysterBalance < 1.00)
+            {
+                throw new Exception("You don't have the minimum balance of 1.00 GBP");
+            }
+            inOysterJourney = true;
+            return inOysterJourney;
+        }
+
+        public bool TouchOut()
+        {
+            inOysterJourney = false;
+            return inOysterJourney;
         }
     }
 
--- OysterCardTests/OysterTests.cs
+++ OysterCardTests/OysterTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using OysterCard;
+using System;
 
 namespace Tests
 {
@@ -28,6 +29,38 @@ namespace Tests
         public void MaxOysterLimit_Returns90_WhenCalled()
         {
             Assert.AreEqual(90.00, oysterCard.maxOysterLimit);
+        }
+
+        [Test]
+        public void OysterTopUp_ThrowsException_WhenExceeding90GBPBalance()
+        {
+            Assert.Throws<Exception>(() => oysterCard.TopUp(85.00));
+        }
+
+        [Test]
+        public void OysterDeduct_Deducts5GBP_WhenPassed5()
+        {
+            Assert.AreEqual(5, oysterCard.Deduct(5.00));
+        }
+
+        [Test]
+        public void InOysterJourney_IsTrue_WhenTouchedIn()
+        {
+            Assert.IsTrue(oysterCard.TouchIn());
+        }
+
+        [Test]
+        public void InOysterJourney_IsFalse_WhenTouchedOut()
+        {
+            oysterCard.TouchIn();
+            Assert.IsFalse(oysterCard.TouchOut());
+        }
+
+        [Test]
+        public void TouchIn_ThrowsException_WhenBalanceBelow1()
+        {
+            oysterCard.Deduct(9.50);
+            Assert.Throws<Exception>(() => oysterCard.TouchIn());
         }
     }
 }
